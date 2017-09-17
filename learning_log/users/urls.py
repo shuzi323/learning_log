a92b7986d5b60f4ac0175470d4a80426fcb7287a
shuzi323 <1747882365@qq.com>
2017-09-17 16:56:36 +0800

    login

---
+++ learning_log/users/urls.py
@@ -0,0 +1,9 @@
+"""为应用程序users定义URL模式"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+]
